9f821453e8f770f56e0a1073abef9b3579645599
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-10-03 22:31:16 -0300

    datasets csv

--- 1_🏡_home.py
+++ 1_🏡_home.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd 
 from datetime import datetime
-import os
 
 st.set_page_config(
     page_title="Home",
@@ -9,13 +8,9 @@ st.set_page_config(
     layout="wide"
 )
 
-
-dataset_path = os.getenv('DATASET_PATH', 'C:\\workspace-python\\fifaplay\\datasets\\CLEAN_FIFA23_official_data.csv')
-
 if "data" not in st.session_state:
   
-    df_data = pd.read_csv(dataset_path, index_col=0)
-    # df_data = pd.read_csv("datasets\CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = pd.read_csv("datasets\CLEAN_FIFA23_official_data.csv", index_col=0)
     df_data = df_data[df_data['Contract Valid Until'] >= datetime.today().year]
     df_data = df_data.sort_values(by='Overall', ascending=False) 
     st.session_state.data = df_data
